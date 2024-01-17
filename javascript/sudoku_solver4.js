0b6d8db92304aa4cf3eec6e2211289e7759772f6
surendar-05 <surendarrockzz7@gmail.com>
2024-01-17 19:28:17 +0530

    Toggler

--- javascript/sudoku_solver4.js
+++ javascript/sudoku_solver4.js
@@ -166,5 +166,22 @@ document.getElementById("resetButton").addEventListener("click", (e) => {
 });
 
 
+//Dark mode to Light Mode
+const toggle = document.getElementById('toggleDark');
+const body = document.querySelector('body');
+
+toggle.addEventListener('click', function(){
+    this.classList.toggle('bi-moon');
+    if(this.classList.toggle('bi-brightness-high-fill')){
+        body.style.background = 'white';
+        body.style.color = 'black';
+        body.style.transition = '2s';
+    }else{
+        body.style.background = 'black';
+        body.style.color = 'white';
+        body.style.transition = '2s';
+    }
+});
+
   
 
