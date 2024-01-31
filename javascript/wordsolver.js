c5c82fd20b2048e86b9ec59d5b7315b4701a6618
Neeraj <neerajantil.antil26@gmail.com>
2024-01-31 23:10:21 +0530

    [ISSUE - 180] Improve performance of Word Unscrambler

--- javascript/wordsolver.js
+++ javascript/wordsolver.js
@@ -1,46 +1,64 @@
-    const englishWords = [
-      // ... (use a larger dictionary)
-      "hand", "had", "dah", "and", "ah", "ha", "ad", "an", "da", "nah",
-      "cat", "bat", "act", "tab", "cab", "can", "tan", "ant", "tan", "nat",
-      // ...
-    ];
+// Function to fetch the dataset from the JSON file
+function fetchDataset() {
+  return fetch("../javascript/wordsolverDataset/dataset.json")
+    .then((response) => response.json())
+    .then((data) => {
+      return data;
+    })
+    .catch((error) => {
+      console.error("Error fetching dataset:", error);
+      return [];
+    });
+}
 
-    function solveWords() {
-      var inputLetters = document.getElementById("inputLetters").value.toLowerCase();
+// Using async/await to handle the asynchronous fetch
+async function solveWords() {
+  var inputLetters = document
+    .getElementById("inputLetters")
+    .value.toLowerCase();
 
-      if (inputLetters.trim() !== "") {
-        var results = {};
+  if (inputLetters.trim() !== "") {
+    try {
+      const englishWords = await fetchDataset();
 
-        for (var i = 0; i < englishWords.length; i++) {
-          var word = englishWords[i];
-          var isValid = true;
+      var results = {};
 
-          for (var j = 0; j < word.length; j++) {
-            if (inputLetters.indexOf(word[j]) === -1) {
-              isValid = false;
-              break;
-            }
-          }
-          if (isValid) {
-            var length = word.length.toString();
-            results[length] = results[length] || [];
-            results[length].push(word);
+      for (var i = 0; i < englishWords.length; i++) {
+        var word = englishWords[i];
+        var isValid = true;
+
+        for (var j = 0; j < word.length; j++) {
+          if (inputLetters.indexOf(word[j]) === -1) {
+            isValid = false;
+            break;
           }
         }
-
-        displayResults(results);
-      } else {
-        alert("Please enter letters!");
-      }
-    }
-
-    function displayResults(results) {
-      var resultElement = document.getElementById("result");
-      resultElement.innerHTML = "";
-
-      for (var length in results) {
-        if (results.hasOwnProperty(length)) {
-          resultElement.innerHTML += `<p>${length} letter: ${results[length].join(", ")}</p>`;
+        if (isValid) {
+          var length = word.length.toString();
+          results[length] = results[length] || [];
+          results[length].push(word);
         }
       }
+
+      displayResults(results);
+    } catch (error) {
+      console.error("Error:", error);
     }
+  } else {
+    alert("Please enter letters!");
+  }
+}
+
+// Function to display the results
+function displayResults(results) {
+  var resultElement = document.getElementById("result");
+  resultElement.innerHTML = "";
+
+  for (var length in results) {
+    if (results.hasOwnProperty(length)) {
+      resultElement.innerHTML += `<li><b>${length} letter:</b> ${results[length].join(
+        ", "
+      )}</li>`;
+    }
+  }
+}
