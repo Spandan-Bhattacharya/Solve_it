0cd3469773c1c595774dbc4119f3c9c114e7db82
Srivarsha <98333833+thesri@users.noreply.github.com>
2024-01-31 23:10:40 +0530

    required changed done on hover effect of contributors page (#181)
    * required changed done on hover effect of contributors page
    
    * brightness added, color effect
    closes #173

--- javascript/contributors.js
+++ javascript/contributors.js
@@ -14,20 +14,57 @@ async function fetchContributors(owner,repo){
     return[];
 }
 
-async function displayContributors(contributors){
+async function displayContributors(contributors) {
     const contributorsList = document.getElementById('contributors-list');
-    for (const contributor of contributors){
+
+    for (const contributor of contributors) {
         const li = document.createElement('li');
-        li.className="contributor-item";
+        li.className = "contributor-item";
+
+        // Define color in JavaScript
+        const hoverColor = 'rgb(152, 93, 16)';
+        const brightnessColor = 'brightness(135%)';
+
         li.innerHTML = `
             <a href="${contributor.html_url}" target="_blank">
-            <img class="contributor-image"src="${contributor.avatar_url}" alt="${contributor.login}" width="100" height="100">
+                <img class="contributor-image" src="${contributor.avatar_url}" alt="${contributor.login}" width="100" height="100">
             </a>
-            <p class="contributor-name">${contributor.login}</p>
+            <p class="contributor-name">
+                <a href="${contributor.html_url}" target="_blank">${contributor.login}</a>
+            </p>
         `;
+
+       // Apply color styles dynamically
+li.addEventListener('mouseover', () => {
+    //li.querySelector('.contributor-name a').style.color = hoverColor;
+    li.querySelector('.contributor-image').style.filter = brightnessColor;
+    li.querySelector('.contributor-image').style.transform = 'scale(1.2)';
+    li.style.transition = '0.3s';
+});
+
+li.addEventListener('mouseout', () => {
+    //li.querySelector('.contributor-name a').style.color = ''; // Reset color to default
+    li.querySelector('.contributor-image').style.filter = ''; // Reset filter to default
+    li.querySelector('.contributor-image').style.transform = ''; // Reset transform to default
+    li.style.transition = ''; // Reset transition to default
+});
+const contributorName = li.querySelector('.contributor-name a');
+
+// Contributor Name event listeners
+contributorName.addEventListener('mouseover', () => {
+    contributorName.style.color = hoverColor;
+    li.style.transition = '0.3s';
+});
+
+contributorName.addEventListener('mouseout', () => {
+    contributorName.style.color = ''; // Reset color to default
+    li.style.transition = ''; // Reset transition to default
+});
+
         contributorsList.append(li);
     }
 }
+
 const owner='Spandan-Bhattacharya';
 const repo='Solve_it';
 fetchContributors(owner,repo)
