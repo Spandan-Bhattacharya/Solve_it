3ed20a8d0fc5f47db88151b04762a3341619060b
prakharsingh-74 <prakharsingh7014@gmail.com>
2024-02-06 22:32:46 +0530

    javascript code updated for the search box

--- javascript/script.js
+++ javascript/script.js
@@ -8,3 +8,22 @@ openMenu.addEventListener('click' , ()=>{
     list.classList.toggle('move');
     body.classList.toggle('scroll');
 })
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    const searchInput = document.getElementById('input-box');
+    const cards = document.querySelectorAll('.box');
+  
+    searchInput.addEventListener('input', function (event) {
+      const searchText = event.target.value.toLowerCase();
+      cards.forEach(function (card) {
+        const cardText = card.textContent.toLowerCase();
+        if (cardText.includes(searchText)) {
+          card.style.display = 'block';
+        } else {
+          card.style.display = 'none';
+        }
+      });
+    });
+  });
+  
