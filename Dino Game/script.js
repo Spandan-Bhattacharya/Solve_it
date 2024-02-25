90a4d2011f38813fd7e39191529767b02885e15b
SOHAN <sohanroychowdhury7@gmail.com>
2024-02-25 14:18:42 +0530

    Made Changes

--- Dino Game/script.js
+++ Dino Game/script.js
@@ -1,10 +1,13 @@
 let dino = document.querySelector(".dino");
 let box = document.querySelector(".obstacle");
 let reset = document.querySelector(".reset");
+let flag = true;
+let score = 0;
+let scoreDiv = document.querySelector(".score");
 
 function jump() {
     if (!dino.classList.contains("animateDino")) {
-        dino.classList.add("animateDino");
+        dino.classList.add("animateDino");  
     }
 
     setTimeout(() => {
@@ -16,16 +19,33 @@ setInterval(() => {
     let dinoPos = parseInt(window.getComputedStyle(dino).getPropertyValue("top"));
     let boxPos = parseInt(window.getComputedStyle(box).getPropertyValue("left"));
 
-    if (boxPos > 0 && boxPos <= 80 && dinoPos >= 350) {
+    // if (boxPos > 0 && boxPos <= 80 && dinoPos >= 350) {
+    //     box.style.display = 'none';
+    //     box.classList.remove("animateBox");
+    //     flag = false;
+    //     alert('Game Over') ;
+    // }
+    if (boxPos > 0 && boxPos <= 50 && dinoPos >= 370) {
         box.style.display = 'none';
         box.classList.remove("animateBox");
-        alert('Game Over');
+        flag = false;
+        alert('Game Over') ;
+    }
+}, 10);
+
+setInterval(() => {
+    if(flag) { 
+        score++;
+        scoreDiv.innerHTML = `Score : ${score}`;
     }
 
-}, 10);
+}, 1200);
 document.addEventListener("keydown", jump);
 
 reset.addEventListener("click", () => {
     box.style.display = 'inline-block';
     box.classList.add("animateBox");
+    scoreDiv.innerHTML = `Score : 0`;
+    score = 0;
+    flag = true;
 })
